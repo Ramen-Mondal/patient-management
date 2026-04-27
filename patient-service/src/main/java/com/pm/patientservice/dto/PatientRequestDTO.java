31f2a243e11faa337fc2dee2b3d4e64b9c4394c6
Ramen Mondal <rmofficial01x@gmail.com>
2026-04-27 12:38:53 +0530

    PatientUpdate service made and according that code done in service, controller also create a PatientNotFoundException class, And learn about @Validated tag, also one problem appear, email exist with same id, but solve that using existsByEmailAndIdNot repository and service code

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.dto;
 
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,36 +9,77 @@ import jakarta.validation.constraints.Size;
 import java.util.Date;
 
 public class PatientRequestDTO {
-    public String getName() {
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//
+//    public String getDateOfBirth() {
+//        return dateOfBirth;
+//    }
+//
+//    public void setDateOfBirth(String dateOfBirth) {
+//        this.dateOfBirth = dateOfBirth;
+//    }
+//
+//    public String getRegisteredDate() {
+//        return registeredDate;
+//    }
+//
+//    public void setRegisteredDate(String registeredDate) {
+//        this.registeredDate = registeredDate;
+//    }
+
+    //Generate Getter and Setter with all the annotation [] tick the copy all the annotation
+    public @NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(@NotBlank(message = "Name is required") @Size(max = 100, message = "Name cannot exceed 100 characters") String name) {
         this.name = name;
     }
 
-    public String getEmail() {
+    public @NotBlank(message = "Email is required") @Email(message = "Email should be valid") String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(@NotBlank(message = "Email is required") @Email(message = "Email should be valid") String email) {
         this.email = email;
     }
 
-    public String getAddress() {
+    public @NotBlank(message = "Address is required") String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(@NotBlank(message = "Address is required") String address) {
         this.address = address;
     }
 
-
-    public String getDateOfBirth() {
+    public @NotBlank(message = "Date of Birth is required") String getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(String dateOfBirth) {
+    public void setDateOfBirth(@NotBlank(message = "Date of Birth is required") String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
@@ -60,10 +102,10 @@ public class PatientRequestDTO {
     @NotBlank(message = "Address is required")
     private String address;
 
-    @NotBlank(message = "Address is required")
+    @NotBlank(message = "Date of Birth is required")
     private String dateOfBirth;
 
-    @NotNull(message = "Registered date is required")
+    @NotBlank(groups = CreatePatientValidationGroup.class, message = "Registered date is required")
     private String registeredDate;
 
 }
